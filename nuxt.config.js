d74e3df756fc09e6874ff5b9ea31b1c81139db4d
kthatoto <kthatoto@gmail.com>
2021-02-08 01:57:36 +0900

    init

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+export default {
+  ssr: false,
+  srcDir: 'src',
+  target: 'static',
+  head: {
+    title: 'tw-mapmaker',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  components: true,
+  buildModules: [
+    '@nuxt/typescript-build'
+  ],
+  modules: [
+    '@nuxtjs/axios'
+  ],
+  axios: {},
+  build: {
+    transpile: [/^element-ui/]
+  }
+}
