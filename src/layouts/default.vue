50fbe410484479923f219434368b7ecbab7b649b
kthatoto <kthatoto@gmail.com>
2021-02-09 02:47:57 +0900

    Add header

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-Nuxt
+.default
+  .header
+    nuxt-link(to="/") Home
+  Nuxt.main
 </template>
 
 <style>
@@ -8,5 +11,32 @@ Nuxt
   padding: 0;
   box-sizing: border-box;
   list-style: none;
+  text-decoration: none;
+}
+a:visited {
+  color: inherit;
+}
+
+.header {
+  width: 100%;
+  height: 50px;
+  background-color: #333333;
+  display: flex;
+  align-items: center;
+}
+.header a {
+  color: white;
+  display: inline-block;
+  width: 100px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  border-right: 1px solid white;
+}
+
+.main {
+  width: 100%;
+  height: calc(100vh - 60px);
+  background-color: lightgray;
 }
 </style>
