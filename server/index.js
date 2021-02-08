844da1e2b5cd838032917ef9bee8ac3c9c2d3387
kthatoto <kthatoto@gmail.com>
2021-02-09 06:00:46 +0900

    Display image list

--- server/index.js
+++ server/index.js
@@ -4,16 +4,11 @@ import fs from "fs"
 const app = express()
 
 app.get("/images", async (req, res) => {
-  const fileList = []
-  await fs.promises.readdir("./data/images", (err, files) => {
-    if (err) throw err
-    files.forEach((file) => {
-      fileList.push(file)
-    })
-    console.log("#01")
-    res.send(fileList)
+  fs.promises.readdir("./data/images", {}).then(files => {
+    res.send(files)
+  }).catch(err => {
+    throw err
   })
-  console.log("#02")
 })
 
 export default {
--- src/components/Console/Images.vue
+++ src/components/Console/Images.vue
@@ -8,18 +8,35 @@
       :show-file-list="false"
     )
       el-button(icon="el-icon-plus" type="primary") 追加
+  img(v-for="image in images" :src="image")
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api"
+import { defineComponent, reactive, toRefs, onMounted } from "@vue/composition-api"
+import axios from "axios"
 
 export default defineComponent({
   setup() {
+    const state = reactive<{
+      images: any[]
+    }>({
+      images: []
+    })
+
+    onMounted(async () => {
+      const imagesRes = await axios.get("/api/images")
+      imagesRes.data.forEach(async (filename: string) => {
+        const image = (await import(`~data/images/${filename}`)).default
+        state.images.push(image)
+      })
+    })
+
     const handleUpload = (files: any[]) => {
       console.log(files)
     }
 
     return {
+      ...toRefs(state),
       handleUpload
     }
   }
