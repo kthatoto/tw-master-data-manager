9a827e2ac8b68ca165229916cfb557879b65d604
kthatoto <kthatoto@gmail.com>
2021-02-10 02:24:15 +0900

    Fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,15 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "vue/return-in-computed-property": "off",
+    "getter-return": "off",
+    "no-dupe-class-members": "off",
+  },
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
 }
