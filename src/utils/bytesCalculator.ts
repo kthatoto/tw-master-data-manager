f6d387ad73c86d0a0f8f99e7d7fb9387739136e3
kthatoto <kthatoto@gmail.com>
2021-02-13 02:07:05 +0900

    Fix type error

--- src/utils/bytesCalculator.ts
+++ src/utils/bytesCalculator.ts
@@ -1,6 +1,6 @@
 export default (bytes: number) => {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
   if (bytes === 0) return '0 Byte'
-  const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1000)))
-  return Math.round(bytes / Math.pow(1000, i), 2) + ' ' + sizes[i]
+  const i = Math.floor(Math.floor(Math.log(bytes) / Math.log(1000)))
+  return Math.round(bytes / Math.pow(1000, i)) + ' ' + sizes[i]
 }
