8759a1520b9791d8954c17dc6023dec68b790873
kthatoto <kthatoto@gmail.com>
2021-02-18 03:34:47 +0900

    Tile create

--- server/tiles/index.ts
+++ server/tiles/index.ts
@@ -1,14 +1,14 @@
 import fs from 'fs'
 
 import getList from './getList'
-// import create from './create'
+import createTile from './createTile'
 import createDirectory from './createDirectory'
 import moveDirectory from './moveDirectory'
 import deleteObject from './deleteObject'
 
 export default (app: any) => {
   getList(app, 'get', '/tiles')
-  // create(app, 'post', '/tiles')
+  createTile(app, 'post', '/tiles')
   createDirectory(app, 'post', '/tiles/directories')
   moveDirectory(app, 'patch', '/tiles/directories')
   deleteObject(app, 'patch', '/tiles/delete')
