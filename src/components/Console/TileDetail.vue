831c640053d8816b8978e8126fa3bd1a29c3c6c1
kthatoto <kthatoto@gmail.com>
2021-02-18 03:25:28 +0900

    Fix

--- src/components/Console/TileDetail.vue
+++ src/components/Console/TileDetail.vue
@@ -3,8 +3,10 @@
   img(:src="showingTile.raw")
   h3 {{ showingTile.name }}
   p サイズ：{{ bytes }}
+  p 衝突：{{ showingTile.collision ? 'あり' : 'なし' }}
+  p 画像：{{ showingTile.imagePath }}
   .buttons
-    el-button(type="primary" @click="openEditModal(refs, showingTile)") 名前変更
+    el-button(type="primary" @click="openEditModal(refs, showingTile)") 変更
     el-button(type="danger" @click="confirmDelete(showingTile.name)") 削除
 </template>
 
