b1d5214867b237c17ab1c8dac4af45f4d1019f5b
kthatoto <kthatoto@gmail.com>
2021-02-22 01:51:02 +0900

    Fix

--- domains/images.ts
+++ domains/images.ts
@@ -1,6 +1,6 @@
 import { Directory, BasicObject } from '~domains/index.ts'
 
-export interface Image extends BasicObject
+export interface Image extends BasicObject {}
 
 export interface ImagesResponse {
   images: Image[]
