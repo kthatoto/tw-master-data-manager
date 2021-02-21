3fd99108ecddd4509f8d7913318ed448e2cdb646
kthatoto <kthatoto@gmail.com>
2021-02-21 17:08:08 +0900

    Add domains and use it

--- server/images/index.ts
+++ server/images/index.ts
@@ -1,12 +1,12 @@
 import getList from './getList'
-import create from './create'
+import createImage from './createImage'
 import move from './move'
 import createDirectory from './createDirectory'
 import deleteObject from './deleteObject'
 
 export default (app: any) => {
   getList(app, 'get', '/images')
-  create(app, 'post', '/images')
+  createImage(app, 'post', '/images')
   move(app, 'patch', '/images')
   createDirectory(app, 'post', '/images/directories')
   deleteObject(app, 'patch', '/images/delete')
