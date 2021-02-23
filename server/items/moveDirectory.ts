a29989b1e27c413aaf457ab26dcd7cec4f70176f
kthatoto <kthatoto@gmail.com>
2021-02-24 02:57:36 +0900

    Add items api

---
+++ server/items/moveDirectory.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+export default (app: any, method: 'patch', path: string) => {
+  app[method](path, async (req: any, res: any) => {
+    const before: string = req.body.before
+    const beforeFilePath = `./data/items${req.query.directory}${before}`
+    const after: string = req.body.after
+    const afterFilePath = `./data/items${req.query.directory}${after}`
+    try {
+      const stat = await fs.promises.stat(beforeFilePath)
+      if (!stat.isDirectory()) {
+        return res.send({ message: `「${before}」はディレクトリではありません` })
+      }
+      if (fs.existsSync(afterFilePath)) {
+        return res.send({ message: `「${after}」は既に存在してます` })
+      }
+      fs.rename(beforeFilePath, afterFilePath, (err: any) => {
+        if (err) throw err
+        res.send(null)
+      })
+    } catch (err: any) {
+      res.send({ message: '把握していない不具合', err })
+    }
+  })
+}
