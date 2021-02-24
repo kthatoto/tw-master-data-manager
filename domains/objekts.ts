a2d575b53a3b8c513013342b6c75e7eba56fc7fa
kthatoto <kthatoto@gmail.com>
2021-02-25 03:22:42 +0900

    Fix objekts

--- domains/objekts.ts
+++ domains/objekts.ts
@@ -1,16 +1,31 @@
 import { Directory, BasicObject } from '~domains/index.ts'
 
+export const objektCategoryLabels = {
+  normal: '通常',
+  chest: 'チェスト'
+}
 export type ObjektCategory = 'normal' | 'chest'
 
 export interface Objekt extends BasicObject {
   collision: boolean
   imagePath: string
+  category: ObjektCategory
+  chest: 
 }
 
 export interface ObjektJson {
   name: string
   collision: boolean
   imagePath: string
+  category: ObjektCategory
+  chest?: {
+    items: {
+      itemPath: string
+      minAmount: number
+      maxAmount: number
+    }[]
+    respawnDurationSecond: number
+  }
 }
 
 export interface ObjektsResponse {
