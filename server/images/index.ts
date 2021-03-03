43a7a8336a6e5392c612e7e1d8d7dcd72987d78e
kthatoto <kthatoto@gmail.com>
2021-03-04 01:39:30 +0900

    Fix server

--- server/images/index.ts
+++ server/images/index.ts
@@ -1,11 +1,9 @@
 import getList from './getList'
 import create from './create'
 import edit from './edit'
-import deleteObject from './deleteObject'
 
 export default (app: any) => {
   getList(app, 'get', '/images')
   create(app, 'post', '/images')
   edit(app, 'patch', '/images')
-  deleteObject(app, 'patch', '/images/delete')
 }
--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,6 @@ import express from 'express'
 import bodyParser from 'body-parser'
 
 import imagesHandle from './images/index'
-import tilesHandle from './tiles/index'
-import objektsHandle from './objekts/index'
-import itemsHandle from './items/index'
 
 import createDirectory from './createDirectory'
 
@@ -14,9 +11,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 imagesHandle(app)
-tilesHandle(app)
-objektsHandle(app)
-itemsHandle(app)
 
 createDirectory(app, 'post', '/directories')
 
