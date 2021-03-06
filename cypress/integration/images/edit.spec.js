e500b8f0fca8a49e03a30d27b8f8cdd7ca78acf5
kthatoto <kthatoto@gmail.com>
2021-03-06 22:54:46 +0900

    Add edit spec

--- cypress/integration/images/edit.spec.js
+++ cypress/integration/images/edit.spec.js
@@ -4,12 +4,26 @@ context('Images Edit', () => {
       const sampleImage = 'field1.png'
       cy.contains('images作成').click()
       cy.wait(100)
-      cy.get('input[type="file"]').attachFile()
+      cy.get('.dialog.-imageCreate input[type="file"]').attachFile()
       cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.wait(100)
       cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
 
+      const editImage = 'field2.png'
       cy.contains('sample.png').dblclick()
+      cy.wait(100)
+      cy.get('.dialog.-imageEdit input[type="file"]').attachFile(editImage)
+      cy.get('.dialog.-imageEdit input.el-input__inner').clear().type('sample2')
+      cy.contains('.dialog.-imageEdit button.el-button', '更新').click()
+      cy.wait(100)
+      cy.contains('更新完了！').should('be.visible')
+
+      cy.contains('sample2.png').should('be.visible')
+      cy.get('.resources__item').should('have.length', 1)
+      cy.fixture(editImage).then((imageSource) => {
+        cy.get('.resources__item img')
+          .invoke('attr', 'src')
+          .should('include', imageSource)
+      })
     })
   })
 })
