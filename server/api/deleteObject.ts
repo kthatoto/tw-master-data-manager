36a9d2bed3ae5a47d9c0eab818e75e8a2736fefb
kthatoto <kthatoto@gmail.com>
2021-03-10 05:11:40 +0900

    Add delete object api

--- server/api/deleteObject.ts
+++ server/api/deleteObject.ts
@@ -1,29 +1,44 @@
-import fs from 'fs'
 import { Application, Request, Response } from 'express'
 
-import { ResourceKey } from '~server/index'
+import { ResourceKey, ResourceModel } from '~server/index'
 import { ResponseMessage, DefaultResponseBody } from '~server/api/index'
+import Image from '../models/image'
 
 export default (app: Application, method: 'delete', path: string) => {
   app[method](path, async (req: Request, res: Response<DefaultResponseBody>) => {
-    const objectPath = req.query.path
-    const fullObjectPath: string = `${app.get('baseDirectory')}/${objectPath}`
-    try {
-      const stat = await fs.promises.stat(fullObjectPath)
-      if (stat.isDirectory()) {
-        await fs.promises.rmdir(fullObjectPath)
+    const resourceKey: ResourceKey = req.query.resourceKey as ResourceKey
+    const path = req.query.path
+    const name = req.query.name
+
+    const Model = {
+      images: Image
+    }[resourceKey]
+    const doc: ResourceModel | null = await Model.findOne({ path, name })
+    if (!doc) {
+      res.send({ message: `「${path}${name}」はありません` })
+      return
+    }
+
+    let result: boolean
+    let message: string = ''
+    if (doc.objectType === 'file') {
+      result = await doc.remove()
+      if (!result) message = '削除に失敗しました'
+    } else if (doc.objectType === 'directory') {
+      const docs: ResourceModel[] = await Model.find({ path: `${path}${name}/` })
+      if (docs.length === 0) {
+        result = await doc.remove()
+        if (!result) message = '削除に失敗しました'
       } else {
-        await fs.promises.unlink(fullObjectPath)
+        result = false
+        message = `「${path}${name}/」の中は空じゃないので消せません`
       }
+    }
+
+    if (result) {
       res.send(null)
-    } catch (err: any) {
-      if (err.code === 'ENOENT') {
-        res.send({ message: `「${objectPath}」は存在しません` })
-      } else if (err.code === 'ENOTEMPTY') {
-        res.send({ message: `「${objectPath}」の中は空じゃないので消せません` })
-      } else {
-        res.send({ message: '把握していない不具合', err })
-      }
+    } else {
+      res.send({ message })
     }
   })
 }
--- src/stores/common_store.ts
+++ src/stores/common_store.ts
@@ -82,8 +82,9 @@ export const buildCommonStore = (stores: AppStores) => {
   }
   const deleteObject = async (key: ResourceKey) => {
     const store = getStoreByKey(key)
-    const path = `${store.currentDirectory.value}${deleteForm.name}`
-    const res = await axios.delete(`/api/objects?resourceKey=${key}&path=${path}`)
+    const path = store.currentDirectory.value
+    const name = deleteForm.name
+    const res = await axios.delete(`/api/objects?resourceKey=${key}&path=${path}&name=${name}`)
     const result: boolean = handleResponse(res, '削除完了！', store.fetchResources, deleteForm)
     if (result) {
       store.showingResourceIndex.value = undefined
