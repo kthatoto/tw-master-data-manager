334068074a8baf248fe2db7825f2de11e1e65c05
kthatoto <kthatoto@gmail.com>
2021-03-12 06:21:40 +0900

    Refactor cypress

--- cypress/integration/images/create.spec.js
+++ cypress/integration/images/create.spec.js
@@ -2,49 +2,30 @@ context('Images Create', () => {
   context('Success', () => {
     it('creates an image', () => {
       const sampleImage = 'field1.png'
-      cy.createImage(sampleImage)
-      cy.wait(100)
-      cy.contains('作成完了！').should('be.visible')
-
+      cy.createImage(sampleImage, 'sample', '作成完了！')
       cy.contains('sample.png').should('be.visible')
-      cy.fixture(sampleImage).then((imageSource) => {
-        cy.get('.resources__item img')
-          .invoke('attr', 'src')
-          .should('include', imageSource)
-      })
+      cy.shouldVisibleImage('.resources__item img', sampleImage)
     })
 
     it('creates an image under a directory', () => {
       const directoryName = 'fields'
       cy.createDirectory(directoryName)
-      cy.wait(100)
       cy.get('.resources__item svg').dblclick()
 
       const sampleImage = 'field1.png'
-      cy.createImage(sampleImage)
-      cy.wait(100)
-      cy.contains('作成完了！').should('be.visible')
+      cy.createImage(sampleImage, 'sample', '作成完了！')
 
       cy.contains('.nav .breadcrumb', directoryName).should('be.visible')
       cy.contains('sample.png').should('be.visible')
-      cy.fixture(sampleImage).then((imageSource) => {
-        cy.get('.resources__item img')
-          .invoke('attr', 'src')
-          .should('include', imageSource)
-      })
+      cy.shouldVisibleImage('.resources__item img', sampleImage)
     })
   })
 
   context('Failure', () => {
     it('fails to create an image because name is duplicate to another image', () => {
       const sampleImage = 'field1.png'
-      cy.createImage(sampleImage)
-      cy.wait(100)
-      cy.contains('作成完了！').should('be.visible')
-
-      cy.createImage(sampleImage)
-      cy.wait(100)
-      cy.contains('は既に存在してます').should('be.visible')
+      cy.createImage(sampleImage, 'sample', '作成完了！')
+      cy.createImage(sampleImage, 'sample', 'は既に存在してます')
       cy.get('.resources__item').should('have.length', 1)
     })
   })
--- cypress/integration/images/edit.spec.js
+++ cypress/integration/images/edit.spec.js
@@ -2,89 +2,41 @@ context('Images Edit', () => {
   context('Success', () => {
     it('edits an image', () => {
       const sampleImage = 'field1.png'
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-imageCreate input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
+      cy.createImage(sampleImage, 'sample')
 
       const editImage = 'field2.png'
-      cy.contains('sample.png').dblclick()
-      cy.wait(100)
-      cy.get('.dialog.-imageEdit input[type="file"]').attachFile(editImage)
-      cy.get('.dialog.-imageEdit input.el-input__inner').clear().type('sample2')
-      cy.contains('.dialog.-imageEdit button.el-button', '更新').click()
-      cy.wait(100)
-      cy.contains('更新完了！').should('be.visible')
+      cy.editImage('sample.png', editImage, 'sample2', '更新完了！')
 
       cy.contains('sample2.png').should('be.visible')
       cy.get('.resources__item').should('have.length', 1)
-      cy.fixture(editImage).then((imageSource) => {
-        cy.get('.resources__item img')
-          .invoke('attr', 'src')
-          .should('include', imageSource)
-      })
+      cy.shouldVisibleImage('.resources__item img', editImage)
     })
 
     it('edits an image under a directory', () => {
       const directoryName = 'fields'
-      cy.contains('フォルダ作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-directoryCreate input.el-input__inner').type(directoryName)
-      cy.contains('.dialog.-directoryCreate button.el-button', '作成').click()
-      cy.wait(100)
+      cy.createDirectory(directoryName)
       cy.get('.resources__item svg').dblclick()
 
       const sampleImage = 'field1.png'
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-imageCreate input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
-      cy.wait(100)
+      cy.createImage(sampleImage, 'sample')
 
       const editImage = 'field2.png'
-      cy.contains('sample.png').dblclick()
-      cy.wait(100)
-      cy.get('.dialog.-imageEdit input[type="file"]').attachFile(editImage)
-      cy.get('.dialog.-imageEdit input.el-input__inner').clear().type('sample2')
-      cy.contains('.dialog.-imageEdit button.el-button', '更新').click()
-      cy.wait(100)
-      cy.contains('更新完了！').should('be.visible')
+      cy.editImage('sample.png', editImage, 'sample2', '更新完了！')
 
       cy.contains('.nav .breadcrumb', directoryName).should('be.visible')
       cy.contains('sample2.png').should('be.visible')
-      cy.fixture(editImage).then((imageSource) => {
-        cy.get('.resources__item img')
-          .invoke('attr', 'src')
-          .should('include', imageSource)
-      })
+      cy.shouldVisibleImage('.resources__item img', editImage)
     })
   })
 
   context('Failure', () => {
     it('fails to edit an image because name is duplicate to another image', () => {
       const sampleImage = 'field1.png'
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-imageCreate input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
-
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-imageCreate input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample2')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
+      cy.createImage(sampleImage, 'sample')
+      cy.createImage(sampleImage, 'sample2')
 
       const editImage = 'field2.png'
-      cy.contains('sample.png').dblclick()
-      cy.wait(100)
-      cy.get('.dialog.-imageEdit input[type="file"]').attachFile(editImage)
-      cy.get('.dialog.-imageEdit input.el-input__inner').clear().type('sample2')
-      cy.contains('.dialog.-imageEdit button.el-button', '更新').click()
-      cy.wait(100)
-      cy.contains('は既に存在してます').should('be.visible')
+      cy.editImage('sample.png', editImage, 'sample2', 'は既に存在してます')
 
       cy.get('.resources__item').should('have.length', 2)
       cy.contains('sample.png').should('be.visible')
