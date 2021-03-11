292fd7ac67856901741b97b3f2d6c44fac1c4cb8
kthatoto <kthatoto@gmail.com>
2021-03-12 05:20:36 +0900

    Refactor cypress

--- cypress/integration/images/create.spec.js
+++ cypress/integration/images/create.spec.js
@@ -2,11 +2,7 @@ context('Images Create', () => {
   context('Success', () => {
     it('creates an image', () => {
       const sampleImage = 'field1.png'
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-imageCreate input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
+      cy.createImage(sampleImage)
       cy.wait(100)
       cy.contains('作成完了！').should('be.visible')
 
@@ -20,19 +16,12 @@ context('Images Create', () => {
 
     it('creates an image under a directory', () => {
       const directoryName = 'fields'
-      cy.contains('フォルダ作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-directoryCreate input.el-input__inner').type(directoryName)
-      cy.contains('.dialog.-directoryCreate button.el-button', '作成').click()
+      cy.createDirectory(directoryName)
       cy.wait(100)
       cy.get('.resources__item svg').dblclick()
 
       const sampleImage = 'field1.png'
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-imageCreate input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
+      cy.createImage(sampleImage)
       cy.wait(100)
       cy.contains('作成完了！').should('be.visible')
 
@@ -49,19 +38,11 @@ context('Images Create', () => {
   context('Failure', () => {
     it('fails to create an image because name is duplicate to another image', () => {
       const sampleImage = 'field1.png'
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('.dialog.-imageCreate input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
+      cy.createImage(sampleImage)
       cy.wait(100)
       cy.contains('作成完了！').should('be.visible')
 
-      cy.contains('images作成').click()
-      cy.wait(100)
-      cy.get('input[type="file"]').attachFile(sampleImage)
-      cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
-      cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
+      cy.createImage(sampleImage)
       cy.wait(100)
       cy.contains('は既に存在してます').should('be.visible')
       cy.get('.resources__item').should('have.length', 1)
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,18 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 import 'cypress-file-upload'
+
+Cypress.Commands.add('createImage', (imageName) => {
+  cy.contains('images作成').click()
+  cy.wait(100)
+  cy.get('.dialog.-imageCreate input[type="file"]').attachFile(imageName)
+  cy.get('.dialog.-imageCreate input.el-input__inner').clear().type('sample')
+  cy.contains('.dialog.-imageCreate button.el-button', '作成').click()
+})
+
+Cypress.Commands.add('createDirectory', (directoryName) => {
+  cy.contains('フォルダ作成').click()
+  cy.wait(100)
+  cy.get('.dialog.-directoryCreate input.el-input__inner').type(directoryName)
+  cy.contains('.dialog.-directoryCreate button.el-button', '作成').click()
+})
