496f79bdf9fb97bdfbf500f36338a1262a710747
kthatoto <kthatoto@gmail.com>
2021-03-12 10:01:00 +0900

    Fix delete directory spec

--- cypress/integration/images/directories/delete.spec.js
+++ cypress/integration/images/directories/delete.spec.js
@@ -1,39 +1,47 @@
+const directoryName = 'fields'
+const subDirectoryName = 'floors'
+
 context('Images Directories Delete', () => {
   context('Success', () => {
     it('deletes a empty directory', () => {
-      const directoryName = 'fields'
-      cy.createDirectory(directoryName)
+      cy.prepareImageResources([
+        { type: 'directory', name: directoryName }
+      ])
+
       cy.deleteDirectory(directoryName, '削除完了！')
+
+      cy.get('.resources__item').should('have.length', 0)
     })
 
     it('deletes a empty directory under a directory', () => {
-      const directoryName = 'fields'
-      cy.createDirectory(directoryName)
-      cy.get('.resources__item svg').dblclick()
-      const subDirectoryName = 'subfields'
-      cy.createDirectory(subDirectoryName)
-      cy.contains('.nav .breadcrumb', directoryName).should('be.visible')
+      cy.prepareImageResources([
+        { type: 'directory', name: directoryName }
+        { type: 'directory', directories: [directoryName], name: subDirectoryName }
+      ])
+
       cy.deleteDirectory(subDirectoryName, '削除完了！')
+
+      cy.imageResourcesShouldBe([
+        { type: 'directory', name: directoryName }
+      ])
+      cy.goDirectory(directoryName)
+      cy.get('.resources__item').should('have.length', 0)
     })
   })
 
   context('Failure', () => {
     it('fails to delete a directory because it contains some items', () => {
-      const directoryName = 'fields'
-      cy.createDirectory(directoryName)
-      cy.get('.resources__item svg').dblclick()
-      const subDirectoryName = 'subfields'
-      cy.createDirectory(subDirectoryName)
-      cy.contains('.nav .breadcrumb', directoryName).should('be.visible')
+      cy.prepareImageResources([
+        { type: 'directory', name: directoryName }
+        { type: 'directory', directories: [directoryName], name: subDirectoryName }
+      ])
 
-      cy.backToHome()
       cy.deleteDirectory(directoryName, 'の中は空じゃないので消せません')
-      cy.contains(directoryName).should('be.visible')
-      cy.get('.resources__item').should('have.length', 1)
-      cy.get('.resources__item svg').dblclick()
-      cy.get('.resources__item').should('have.length', 1)
-      cy.contains('.nav .breadcrumb', directoryName).should('be.visible')
-      cy.contains(subDirectoryName).should('be.visible')
+
+      cy.imageResourcesShouldBe([
+        { type: 'directory', name: directoryName },
+        { type: 'directory', directories: [directoryName], name: subDirectoryName }
+      ])
     })
   })
 })
