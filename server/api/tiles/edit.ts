9d366f1c53ccd6352f3952639670a6be31dc660e
kthatoto <kthatoto@gmail.com>
2021-03-28 11:31:45 +0900

    Fix tile edit api

--- server/api/tiles/edit.ts
+++ server/api/tiles/edit.ts
@@ -2,9 +2,9 @@ import { Application, Request, Response } from 'express'
 import { Types } from 'mongoose'
 
 import { DefaultResponseBody } from '~server/api/index'
-import Image from '../../models/image'
+import Tile from '../../models/tile'
 
-export interface ImagesEditRequestBody {
+export interface TilesEditRequestBody {
   path: string
   beforeName: string
   name: string
@@ -13,20 +13,20 @@ export interface ImagesEditRequestBody {
 }
 
 export default (app: Application, method: 'patch', path: string) => {
-  app[method](path, async (req: Request<any, any, ImagesEditRequestBody>, res: Response<DefaultResponseBody>) => {
+  app[method](path, async (req: Request<any, any, TilesEditRequestBody>, res: Response<DefaultResponseBody>) => {
     const { path, beforeName, name, collision, imageId } = req.body
 
-    const target: boolean = await Image.exists({ path, name: beforeName, objectType: 'file' })
+    const target: boolean = await Tile.exists({ path, name: beforeName, objectType: 'file' })
     if (!target) {
       res.send({ message: `「${path}${beforeName}」は存在しません` })
       return
     }
-    const already: boolean = await Image.exists({ path, name, objectType: 'file' })
+    const already: boolean = await Tile.exists({ path, name, objectType: 'file' })
     if (already) {
       res.send({ message: `「${path}${name}」は既に存在してます` })
       return
     }
-    await Image.findOneAndUpdate(
+    await Tile.findOneAndUpdate(
       { path, name: beforeName },
       { $set: { name, collision, imageId } }
     )
