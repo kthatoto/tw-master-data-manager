165774424fa977d3ce79698cc9ebffe3b3691e0a
kthatoto <kthatoto@gmail.com>
2021-04-01 07:45:07 +0900

    Fix flag

--- src/components/resourceSelectors/ImageSelector.vue
+++ src/components/resourceSelectors/ImageSelector.vue
@@ -3,11 +3,11 @@
   h3(slot="title") Image選択
   Images(:editable="false")
   div(slot="footer")
-    el-button(type="primary" @click="selectImage") 選択
+    el-button(type="primary" @click="selectImage" :disabled="!selectable") 選択
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
+import { defineComponent, computed } from '@vue/composition-api'
 
 import { appStores } from '@/stores/appStores.ts'
 import Images from '@/components/MapResourcesConsole/Images.vue'
@@ -21,7 +21,14 @@ export default defineComponent({
       context.emit('select', imagesStore.selectingResource.value)
     }
 
-    return { selectImage }
+    const selectable = computed<boolean>(() =>
+      imagesStore.selectingResource.value
+    )
+
+    return {
+      selectImage,
+      selectable
+    }
   }
 })
 </script>
