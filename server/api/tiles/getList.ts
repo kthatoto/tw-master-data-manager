fab6337815126a03e7002f5f6ed89bad8348a37d
kthatoto <kthatoto@gmail.com>
2021-04-04 15:59:22 +0900

    Add tile image

--- server/api/tiles/getList.ts
+++ server/api/tiles/getList.ts
@@ -2,6 +2,7 @@ import { Application, Request, Response } from 'express'
 
 import { TilesResponse } from '~domains/tiles.ts'
 import Tile, { ITile } from '../../models/tile'
+import Image, { IImage } from '../../models/image'
 
 export default (app: Application, method: 'get', path: string) => {
   app[method](path, async (req: Request, res: Response<TilesResponse>) => {
@@ -9,14 +10,16 @@ export default (app: Application, method: 'get', path: string) => {
     const directoryPath: string = req.query.directory as string
 
     const tiles: ITile[] = await Tile.find({ path: directoryPath })
-    tiles.forEach((tile: ITile) => {
+    tiles.forEach(async (tile: ITile) => {
       if (tile.objectType === 'file') {
+        const image: IImage | null = await Image.findOne({ id: tile.imageId })
         response.resources.push({
           id: tile.id,
           path: tile.path,
           name: tile.name,
           collision: tile.collision,
-          imageId: tile.imageId
+          imageId: tile.imageId,
+          data: image ? image.data : undefined
         })
       } else if (tile.objectType === 'directory') {
         response.directories.push({
