c56b7491fc2863cb2b5a6ec631153deee85838bb
kthatoto <kthatoto@gmail.com>
2021-05-03 04:15:44 +0900

    Fix

--- cypress/support/commands/directories.js
+++ cypress/support/commands/directories.js
@@ -1,8 +1,8 @@
 Cypress.Commands.add('createDirectory', (type, directoryName, expectedMessage) => {
   cy.get(`.${type}`).contains('フォルダ作成').click()
   cy.wait(100)
-  cy.get('.dialog.-directoryCreate input.el-input__inner').type(directoryName)
-  cy.contains('.dialog.-directoryCreate button.el-button', '作成').click()
+  cy.get(`.${type} .dialog.-directoryCreate input.el-input__inner`).type(directoryName)
+  cy.contains(`.${type} .dialog.-directoryCreate button.el-button`, '作成').click()
   cy.wait(100)
   if (expectedMessage) {
     cy.contains(expectedMessage).should('be.visible')
@@ -12,8 +12,8 @@ Cypress.Commands.add('createDirectory', (type, directoryName, expectedMessage) =
 Cypress.Commands.add('editDirectory', (type, beforeDirectoryName, directoryName, expectedMessage) => {
   cy.get(`.${type}`).contains(beforeDirectoryName).dblclick()
   cy.wait(100)
-  cy.get('.dialog.-directoryEdit input.el-input__inner').clear().type(directoryName)
-  cy.contains('.dialog.-directoryEdit button.el-button', '更新').click()
+  cy.get(`.${type} .dialog.-directoryEdit input.el-input__inner`).clear().type(directoryName)
+  cy.contains(`.${type} .dialog.-directoryEdit button.el-button`, '更新').click()
   cy.wait(100)
   if (expectedMessage) {
     cy.contains(expectedMessage).should('be.visible')
@@ -23,7 +23,7 @@ Cypress.Commands.add('editDirectory', (type, beforeDirectoryName, directoryName,
 Cypress.Commands.add('deleteDirectory', (type, directoryName, expectedMessage) => {
   cy.get(`.${type}`).contains('.resources__item', directoryName).find('svg').rightclick({ multiple: true })
   cy.wait(100)
-  cy.contains('.dialog.-objectDelete button.el-button', '削除').click()
+  cy.contains(`.${type} .dialog.-objectDelete button.el-button`, '削除').click()
   cy.wait(100)
   if (expectedMessage) {
     cy.contains(expectedMessage).should('be.visible')
