02a9c1c307ee6f320b512c4ab9741991bc15b480
kthatoto <kthatoto@gmail.com>
2021-05-05 01:28:32 +0900

    Fix images API

--- server/api/images/create.ts
+++ server/api/images/create.ts
@@ -1,24 +1,24 @@
 import { Application, Request, Response } from 'express'
 
 import { DefaultResponseBody } from '~server/api/index'
-import Image from '../../models/image'
+import ImageModel from '../../models/image'
 
 export interface ImagesCreateRequestBody {
-  path: string
   name: string
   data: string
+  directoryId?: string
 }
 
 export default (app: Application, method: 'post', path: string) => {
   app[method](path, async (req: Request<any, any, ImagesCreateRequestBody>, res: Response<DefaultResponseBody>) => {
-    const { path, name, data } = req.body
+    const { name, data, directoryId } = req.body
 
-    const already: boolean = await Image.exists({ path, name })
+    const already: boolean = await ImageModel.exists({ name, directoryId })
     if (already) {
-      res.send({ message: `「${path}${name}」は既に存在してます` })
+      res.send({ message: `「${name}」は既に存在してます` })
       return
     }
-    const newImage = new Image({ name, path, data, objectType: 'file' })
+    const newImage = new ImageModel({ name, data, directoryId })
     await newImage.save()
     res.send(null)
   })
