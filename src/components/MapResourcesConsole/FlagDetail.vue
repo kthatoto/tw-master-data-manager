22ac1960e0f49abf40917984bf6b22d10de7b68a
kthatoto <kthatoto@gmail.com>
2021-06-06 20:18:03 +0900

    Add flag detail

---
+++ src/components/MapResourcesConsole/FlagDetail.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+.detail
+  ConsoleImage(:resource="showingResource" width="100%" resourceType="flags")
+  h3 {{ showingResource.name }}
+
+  table
+    tr
+      th key
+      td {{ showingResource.key }}
+    tr
+      th description
+      td {{ showingResource.key }}
+
+  .buttons(v-if="editable")
+    el-button(type="primary" @click="openResourceEditModal(showingResource)") 変更
+    el-button(type="danger" @click="confirmDelete(showingResource)") 削除
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+import { appStores } from '@/stores/appStores.ts'
+import ConsoleImage from '@/components/atoms/ConsoleImage.vue'
+
+export default defineComponent({
+  components: { ConsoleImage },
+  props: {
+    refs: {
+      type: Object,
+      required: true
+    },
+    editable: {
+      type: Boolean,
+      required: false,
+      default: true
+    }
+  },
+  setup (props) {
+    const commonStore = appStores.commonStore
+    const flagsStore = appStores.flagsStore
+
+    return {
+      ...commonStore,
+      ...flagsStore
+    }
+  }
+})
+</script>
+
+<style lang="stylus" scoped>
+console-detail()
+</style>
