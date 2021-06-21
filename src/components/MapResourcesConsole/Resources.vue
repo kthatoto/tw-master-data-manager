9bc52d77b88d3d67bf47e9848ce5185c1ff464af
kthatoto <kthatoto@gmail.com>
2021-06-22 06:13:51 +0900

    Fix

--- src/components/MapResourcesConsole/Resources.vue
+++ src/components/MapResourcesConsole/Resources.vue
@@ -89,7 +89,7 @@ export default defineComponent({
         const directoryNames = paths[2]
         await store.fetchResources({ directoryNames })
         const resourceName = paths[3]
-        store.showResourceByName(resourceName)
+        if (resourceName) store.showResourceByName(resourceName)
       }
     })
 
