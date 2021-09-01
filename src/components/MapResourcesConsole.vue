b9516a813d88045373c927be0c5c756b6b18b218
kthatoto <kthatoto@gmail.com>
2021-09-01 13:01:45 +0900

    Add console tabs

--- src/components/MapResourcesConsole.vue
+++ src/components/MapResourcesConsole.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
 .console
-  el-tabs(type="border-card" :value="tab" :before-leave="beforeTabLeave")
+  el-tabs(type="border-card" :value="resourceTab" :before-leave="beforeTabLeave")
     el-tab-pane(label="Images" name="images")
     el-tab-pane(label="Tiles" name="tiles")
     el-tab-pane(label="Flags" name="flags")
-    NuxtChild.pane(:editable="true")
+    NuxtChild.resource-pane(:editable="true")
+  el-tabs.console-tabs(type="border-card" :value="consoleTab")
+    el-tab-pane(label="Select" name="select")
+      .console-pane
+    el-tab-pane(label="Layers" name="layers")
+      .console-pane
 </template>
 
 <script lang="ts">
@@ -14,16 +19,17 @@ import { appStores } from '@/stores/appStores'
 
 export default defineComponent({
   setup (_, context) {
-    const tab = ref<string>('images')
+    const resourceTab = ref<string>('images')
+    const consoleTab = ref<string>('')
 
     const routeName = context.root.$route.name!
     if (routeName.split('-').length === 2) {
-      tab.value = routeName.split('-')[1].toLowerCase()
+      resourceTab.value = routeName.split('-')[1].toLowerCase()
     }
 
     const beforeTabLeave = (newTabName: string, oldTabName: string) => {
-      if (tab.value === newTabName) return true
-      tab.value = newTabName
+      if (resourceTab.value === newTabName) return true
+      resourceTab.value = newTabName
       appStores.clearStoreByName(oldTabName)
       return new Promise<void>((resolve, reject) => {
         return context.root.$router
@@ -34,7 +40,8 @@ export default defineComponent({
     }
 
     return {
-      tab,
+      resourceTab,
+      consoleTab,
       beforeTabLeave
     }
   }
@@ -44,6 +51,8 @@ export default defineComponent({
 <style lang="stylus" scoped>
 .console
   height: 100%
+  display: flex
+  flex-direction: column
   >>> .el-tabs
     &__content
       padding: 0
@@ -51,6 +60,10 @@ export default defineComponent({
       padding: 0 10px
     &--top.el-tabs--border-card>.el-tabs__header .el-tabs__item:nth-child(2)
       padding-left: 10px
-  .pane
-    height: 60vh
+
+  .resource-pane
+    height: 50vh
+
+  .console-tabs
+    flex: 1
 </style>
